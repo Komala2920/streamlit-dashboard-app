1decccb45571689ea51829a75f4e083e06956ace
Komala2920 <talisettikomali@gmail.com>
2025-09-18 14:41:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-with open("assets/style.css") as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+import streamlit as st
+
+# Load custom CSS
+try:
+    with open("assets/style.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+except FileNotFoundError:
+    st.warning("Custom style not applied. 'style.css' not found.")
