59af0f05fbceed96ea25678db5e2ef7990f8f366
Komala2920 <talisettikomali@gmail.com>
2025-09-18 14:43:49 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-# Load custom CSS
-try:
-    with open("assets/style.css") as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-except FileNotFoundError:
-    st.warning("Custom style not applied. 'style.css' not found.")
