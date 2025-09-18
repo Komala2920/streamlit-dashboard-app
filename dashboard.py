44a1fcff189348c616da6f7b425f8fa6de634c50
Komala2920 <talisettikomali@gmail.com>
2025-09-18 14:06:16 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,7 @@ import requests
 st.title("GitHub Dashboard")
 username = st.text_input("Enter GitHub Username")
 if username:
-    response = requests.get(f"https://api.github.com/users/{username}")
+    response = requests.get(f"https://app.powerbi.com/groups/me/reports/4d41c1bc-17bb-491e-8da8-861aaede731f/24434bd2ed4071702132?redirectedFromSignup=1&experience=power-bi")
     if response.status_code == 200:
         data = response.json()
         st.write(f"Name: {data['name']}")
