663787258903e954793dfb866c49e22122467db4
Komala2920 <talisettikomali@gmail.com>
2025-09-18 14:07:47 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,2 +1,2 @@
 st.title("Welcome to GitHub Dashboard App")
-st.image("assets/background.jpg", use_column_width=True)
+st.image("C:\Users\91995\Downloads\cbcb74d7c06b56b3249ef31f5aa9e6a2.jpg", use_column_width=True)
