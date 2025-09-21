a288ff3b625dd4fa953ac734396cd469444978e2
Komala2920 <talisettikomali@gmail.com>
2025-09-21 08:08:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -270,7 +270,7 @@ elif st.session_state.user is not None:
         st.header("👤 Edit Profile")
         
        # Profile Card
-        st.markdown('<div class="card" style="width:60%; margin:auto;">', unsafe_allow_html=True)
+        st.markdown('<div class="card">', unsafe_allow_html=True)
         with st.form("profile_form"):
             col_left, col_right = st.columns(2)
             with col_left:
@@ -339,5 +339,6 @@ elif st.session_state.user is not None:
             st.dataframe(feedback_df)
         else:
             st.info("You haven't submitted any feedback yet.")         
+
 
 
