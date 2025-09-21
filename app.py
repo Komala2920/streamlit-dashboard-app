bc12fe7864d6cc25cf492dd28814e7b92fdfd6a6
Komala2920 <talisettikomali@gmail.com>
2025-09-21 15:17:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -174,9 +174,6 @@ if st.session_state.user is None and st.session_state.page not in ["forgot_passw
         unsafe_allow_html=True
     )
 
-    # --- Lottie Animation: Person Logging In ---
-    st_lottie_url("https://assets2.lottiefiles.com/packages/lf20_ta8z3psv.json", height=200)
-
     username = st.text_input("Username", key="login_user")
     password = st.text_input("Password", type="password", key="login_pass")
     if st.button("Sign In"):
@@ -418,6 +415,7 @@ elif st.session_state.user is not None:
             st.dataframe(feedback_df)
         else:
             st.info("You haven't submitted any feedback yet.")        
+
 
 
 
